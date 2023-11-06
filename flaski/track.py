eb82e830483e7b1fd2f8c9705cb4a9d79beee8b7
pochetes11 <matiasarielmosquera@gmail.com>
2023-11-06 17:06:24 -0300

    track

--- flaski/track.py
+++ flaski/track.py
@@ -12,9 +12,12 @@ bp = Blueprint('track', __name__, url_prefix="/track")
 def index():
     db = get_db()
     tracks = db.execute(
-        """SELECT t.name as titulo,al.Title as cancion FROM tracks t
-                JOIN albums al ON al.AlbumId = t.AlbumId
-                JOIN artists a ON a.ArtistId = al.ArtistId"""
+        """SELECT t.name AS canciones, g.name AS genero, ar.name AS artista, mt.name AS formato, a.title AS titulo
+                FROM tracks t JOIN albums a ON t.AlbumId = a.AlbumId
+                JOIN genres g ON g.GenreId = t.GenreId 
+                JOIN artists ar ON ar.ArtistId = a.ArtistId
+                JOIN media_types mt ON mt.MediaTypeId = t.MediaTypeId
+                ORDER BY t.name DESC"""
     ).fetchall()
     return render_template('track/index.html', tracks=tracks)
 
