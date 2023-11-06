32d51f2b54282248172a3f60b82d073aa0994803
pochetes11 <matiasarielmosquera@gmail.com>
2023-11-06 17:04:44 -0300

    album

--- flaski/alb.py
+++ flaski/alb.py
@@ -12,9 +12,11 @@ bp = Blueprint('alb', __name__, url_prefix="/album")
 def index():
     db = get_db()
     album = db.execute(
-        """SELECT t.name as titulo,al.Title as cancion FROM tracks t
-                JOIN albums al ON al.AlbumId = t.AlbumId
-                JOIN artists a ON a.ArtistId = al.ArtistId"""
+        '''SELECT t.name AS canciones, title AS dvd, ar.name AS artista, g.name AS genero
+         FROM tracks t JOIN albums a ON t.albumId = a.albumId
+         JOIN artists ar ON ar.ArtistId = a.ArtistId
+         JOIN genres g ON g.GenreId = t.GenreId
+         ORDER BY t.name DESC'''
     ).fetchall()
     return render_template('album/index.html', album=album)
 
