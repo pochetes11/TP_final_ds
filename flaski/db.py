424038d285f6a9bcaaf038623a1400fa2e1ffd5a
pochetes11 <matiasarielmosquera@gmail.com>
2023-12-04 13:04:23 -0300

    db.py

--- flaski/db.py
+++ flaski/db.py
@@ -1,38 +1,44 @@
-import sqlite3
-
+import psycopg2
+import psycopg2.extras
 import click
 from flask import current_app, g
 
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+        conn = psycopg2.connect(host="127.0.0.1",
+            database="chinook",
+            user="postgres",
+            password="postgres",
+            port=5432)
+        g.conn = conn
+        g.db = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
     return g.db
 
+
 def close_db(e=None):
     db = g.pop('db', None)
+    conn = g.pop('conn', None)
 
     if db is not None:
         db.close()
 
+    if conn is not None:
+        conn.close()
+
 def init_db():
     db = get_db()
 
-    with current_app.open_resource('db.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+    with current_app.open_resource('schema.sql') as f:
+        db.execute(f.read().decode('utf8'))
 
 
 @click.command('init-db')
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
-    click.echo('Initialized the database.')
-
+    click.echo('Se inició la db.')
 
 def init_app(app):
     app.teardown_appcontext(close_db)
